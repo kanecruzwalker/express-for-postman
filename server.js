9bd7d4b52d4e941d6f86709ee23711e8a5545b25
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-07-03 15:14:50 -0700

    added a mock db, to be able to make sure our posting works

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const app = express();
 
+const database = {
+    users: [
+        {
+            id:'123',
+            name: 'Mr',
+            email: 'mr@email.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id:'456',
+            name: 'Ms',
+            email: 'ms@email.com',
+            password: 'cakes',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+
 app.get('/', (req,res) => {
     res.send("Root Page Works");
 })
