c5f690898dc1d1888419d6e29fd39b2868bc06f0
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-07-04 21:15:56 -0700

    completely functional for boiler plate production

--- server.js
+++ server.js
@@ -22,49 +22,27 @@ db.select('*').from('users').then(data => {
 app.use(bodyParser.json());
 app.use(cors())
 
-const database = {
-    users: [
-        {
-            id:'123',
-            name: 'Mr',
-            email: 'mr@email.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id:'456',
-            name: 'Ms',
-            email: 'ms@email.com',
-            password: 'cakes',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login : [
-        {
-            id: '123',
-            hash: '',
-            email: 'mr@email.com',
-        }
-
-    ]
-}
-
-
 app.get('/', (req,res) => {
     res.send(database.users);
 })
 
 app.post('/signin', (req,res) => {
-
-
-    if (req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password){
-            res.json(database.users[0]);
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash); // true
+        if(isValid){
+            return db.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'))
         }else{
-            res.status(400).json("error logging in");
+            res.status(400).json('wrong credentials')
         }
+    })
+    .catch(err => res.status(400).json('wrong credentails'))
 })
 
 app.post('/register', (req,res) => {
