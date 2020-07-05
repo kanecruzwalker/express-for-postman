de24287fc50a60e19d56991da10192caed070a72
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-07-05 12:43:14 -0700

    added a controllers folder to seperate the logic from route

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require('cors');
 const app = express();
-const knex = require('knex')
+const knex = require('knex');
+const register = require('./controllers/Register');
 
 const db = knex({
     client: 'pg',
@@ -45,32 +46,7 @@ app.post('/signin', (req,res) => {
     .catch(err => res.status(400).json('wrong credentails'))
 })
 
-app.post('/register', (req,res) => {
-    const { email, name, password } = req.body
-    const hash = bcrypt.hashSync(password);
-        db.transaction(trx => {
-            trx.insert({
-              hash: hash,
-              email: email  
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail=>{
-                return trx('users')
-                .returning('*')
-                .insert({
-                    email: loginEmail[0],
-                    name: name,
-                    joined: new Date()
-                }).then(user => {
-                    res.json(user[0]);
-                })
-            })
-                .then(trx.commit)
-                .catch(trx.rollback)
-        })
-        .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
 app.get('/profile/:id', (req,res) => {
     const { id } = req.params;
