037e57599ae7bf29cf342496b8263db42d4ff9ee
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-07-05 14:14:27 -0700

    adding dynamic port

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
 
 
 
-app.listen(3000, ()=> {
-    console.log("Server listening on PORT 3000");
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`Server listening on ${PORT}`);
 })
