3c6e48f76f896dc2b0aa528460234d0c0191abb2
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-07-05 13:41:09 -0700

    added clarifai api key to server to keep key more secure and off of the front end

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,20 @@
+const Clarifai = require('clarifai');
+const { json } = require('body-parser');
+
+const app = new Clarifai.App({
+    apiKey: '95d1e072cc6844acb57382cdd12dbe35'});
+
+const handleApiCall = (req, res) => {
+    app.models
+    .predict(
+        'a403429f2ddf4b49b307e318f00e528b',
+        req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'));
+}
+
 const handleImage = (req,res, db) =>{
     const { id } = req.body;
 
@@ -11,5 +28,6 @@ const handleImage = (req,res, db) =>{
 }
 
 module.exports = {
-    handleImage : handleImage
+    handleImage : handleImage,
+    handleApiCall : handleApiCall
 }
--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 
 app.put('/image', (req,res) => {image.handleImage(req, res, db)})
 
-
+app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
 
 
 
