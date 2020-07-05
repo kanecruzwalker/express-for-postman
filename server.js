5d352e79124a851384bef384f7635866ed64f4bd
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-07-05 12:52:54 -0700

    refactored profile

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const app = express();
 const knex = require('knex');
 const register = require('./controllers/Register');
-const signin = require('./controllers/Signin');
 const Signin = require("./controllers/Signin");
+const profile = require('./controllers/profile');
 
 const db = knex({
     client: 'pg',
@@ -33,22 +33,7 @@ app.post('/signin', (req, res) => {Signin.handleSignin(req, res, db, bcrypt)})
 
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
-app.get('/profile/:id', (req,res) => {
-    const { id } = req.params;
-
-    db.select('*').from('users').where({
-        id: id
-    })
-    .then(user => {
-        if (user.length){
-            res.json(user[0]);
-        }else{
-            res.status(400).json('Not Found')
-        }
-    })
-    .catch(err => res.status(400).json('error getting user'));
-
-})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 
 app.put('/image', (req,res) =>{
     const { id } = req.body;
