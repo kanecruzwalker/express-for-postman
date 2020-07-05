8fddec5239ec176da74c32d00f0084984d4569f8
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-07-04 20:57:43 -0700

    hashing user's password and submiting email and password to login table to be used later, transaction was implemented to do multiple db manipulations at once

--- server.js
+++ server.js
@@ -58,12 +58,6 @@ app.get('/', (req,res) => {
 
 app.post('/signin', (req,res) => {
 
-    // bcrypt.compare("bacon", hash, function(err, res) {
-    //      res == true
-    // });
-    // bcrypt.compare("veggies", hash, function(err, res) {
-    //      res = false
-    // });
 
     if (req.body.email === database.users[0].email && 
         req.body.password === database.users[0].password){
@@ -75,21 +69,29 @@ app.post('/signin', (req,res) => {
 
 app.post('/register', (req,res) => {
     const { email, name, password } = req.body
-
-    db('users')
-    .returning('*')
-    .insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    }).then(user => {
-        res.json(user[0]);
-    }).catch(err => res.status(400).json('unable to register'))
-    
-    
-    // bcrypt.hash(password, null, null, function(err, hash) {
-    //     console.log(hash);
-    // });
+    const hash = bcrypt.hashSync(password);
+        db.transaction(trx => {
+            trx.insert({
+              hash: hash,
+              email: email  
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail=>{
+                return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0],
+                    name: name,
+                    joined: new Date()
+                }).then(user => {
+                    res.json(user[0]);
+                })
+            })
+                .then(trx.commit)
+                .catch(trx.rollback)
+        })
+        .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req,res) => {
